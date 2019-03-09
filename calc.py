59426622e57487a4bc18340783fd53b248fc5ce2
Metrier <45649081+Metrier@users.noreply.github.com>
2019-03-09 10:12:48 +0000

    Update calc
    Added "^"

--- calc.py
+++ calc.py
@@ -13,4 +13,27 @@ if not s[0] == 0 and not s[1] == 0:
     print("Answers")
     nums(s[0], s[1])
 else:
-    print("Actions with zero are meaningless")
+    print("Actions with zero are meaningless")def nums(x, y):
+   print(x,"+",y,"=",x+y)
+   print(x,"-",y,"=",x-y)
+   print(x,"*",y,"=",x*y)
+   print(x,"/",y,"=",x/y)
+   print(x,"^",y,"=",x**y)
+   print()
+s = [0, 0]
+def vvod():
+   s[0] = float(input("Enter first num: "))
+   s[1] = float(input("\nEnter second num: "))
+   if not s[1] == 0:
+       print("\nAnswers:")
+       nums(s[0], s[1])
+   else:
+       print("\nHello world!")
+
+   print("Return to enter nums?\n(y/press enter)")
+   ex = input()
+   if ex == "y" or ex == "yes":
+      vvod()
+   else:
+      exit
+vvod()
