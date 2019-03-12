4025edac0876e6094e755ce897a8ba146600df0c
Metrier <45649081+Metrier@users.noreply.github.com>
2019-03-12 07:04:15 +0000

    Add files via upload

--- calc.py
+++ calc.py
@@ -1,24 +1,46 @@
-def nums(x, y):
-   print(x,"+",y,"=",x+y)
-   print(x,"-",y,"=",x-y)
-   print(x,"*",y,"=",x*y)
-   print(x,"/",y,"=",x/y)
-   print(x,"^",y,"=",x**y)
-   print()
-s = [0, 0]
+#Programm for Termux!
+#For sololearn's inhabitans:
+#you must enter numbers like this:
+#4
+#2
+#but if you enter after the numbers any key, but not "y", the program will close without error.
 def vvod():
-   s[0] = float(input("Enter first num: "))
-   s[1] = float(input("\nEnter second num: "))
-   if not s[1] == 0:
-       print("\nAnswers:")
-       nums(s[0], s[1])
-   else:
-       print("\nHello world!")
+    try:
+        s[0]=float(input("Enter first number: "))
+        s[1]=float(input("Enter second number: "))
+    except ValueError:
+        print("Value error! Enter a NUMBER.")
+        vvod()
+    if s[0] == 0 or s[1] == 0:
+        print("\nPlease exclude zero.")
+        vvod()
+    else:
+        print("\nAnswers:")
+        nums(s[0], s[1])
 
-   print("Return to enter nums?\n(y/press enter)")
-   ex = input()
-   if ex == "y" or ex == "yes":
-      vvod()
-   else:
-      exit
+    print("Return to enter numbers?\n(y/press enter)")
+    ex = input()
+    if ex == "y" or ex == "yes":
+       vvod()
+    else:
+       exit
+def nums(x, y):
+    print(x,"+",y,"=",x+y)
+    print(x,"-",y,"=",x-y)
+    print(x,"*",y,"=",x*y)
+    print(x,"/",y,"=",x/y)
+    try:
+        print(x,"^",y,"=",x**y)
+    except:
+        print(x,"^",y,"= very long number")
+    print(y,"degree root of",x,"=",x**(1/y))
+    print("-------Reverse numbers-------")
+    print(y,"-",x,"=",y-x)
+    print(y,"/",x,"=",y/x)
+    try:
+        print(y,"^",x,"=",y**x)
+    except:
+        print(y,"^",x,"= very long number")
+    print (x,"degree root of",y,"=",y**(1/y))
+s=[0, 0]
 vvod()
